43a14c19debfb8810d7568a0dd406948279aac7b
Collin <collin@Zipcoders-MacBook-Pro.local>
2022-11-28 10:28:22 -0500

    working i think for all cases

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -7,7 +7,7 @@ public class OnBoardComputer implements BurnStream {
         //if (status.getAltitude() < 1000 || status.getVelocity() > 1500) {
 
             //a = (speed * speed)/(altitude * 2)
-            burn = status.getVelocity() * status.getVelocity() / (2 * status.getAltitude()) + 99;
+            burn = status.getVelocity() * status.getVelocity() / (2 * status.getAltitude()) + 100;
             //100 burn does not change velocity of rocket
 
         System.out.println(burn); /*hack!*/
